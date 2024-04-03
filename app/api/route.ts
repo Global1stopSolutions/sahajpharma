53006bc4fa3db1b224618263270debbc159cd61f
Harshit Patel <harshit3228@gmail.com>
2024-04-04 01:23:42 +0530

    Merge

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    // Process a POST request
-  } else {
-    // Handle any other HTTP method
+export async function POST(request: NextRequest) {
+  const { params, operation } = await request.json();
+  return await handlePostOperation(operation, params);
+}
+
+async function handlePostOperation(operation: string, params: any) {
+  switch (operation) {
+    case "RegistrationForm":
+      return registrationForm(params);
+    default:
+      return NextResponse.json({
+        success: false,
+        message: `Operation "${operation}" not supported.`,
+      });
   }
+}
+
+async function registrationForm(params: any) {
+  return NextResponse.json({ success: true, message: "Operation 1 completed", data: params });
 }
